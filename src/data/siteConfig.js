670f156cc6b6b563d26459342751c6c0be980554
smileravikant24-lab <smileravikant24@gmail.com>
2026-09-14 14:13:32 +0530

    fix: add google apps script endpoint for contact form

--- src/data/siteConfig.js
+++ src/data/siteConfig.js
@@ -3,5 +3,5 @@ export const siteConfig = {
   logoUrl: 'https://lh3.googleusercontent.com/d/1LqA0AlGe3VQyRK-ijFcgr1BXJjQFx5P-',
   instagramUrl: 'https://www.instagram.com/Namrata_art_5113/',
   instagramMessageUrl: 'https://ig.me/m/Namrata_art_5113',
-  contactSheetEndpoint: import.meta.env.VITE_CONTACT_SHEET_ENDPOINT || '',
+  contactSheetEndpoint: import.meta.env.VITE_CONTACT_SHEET_ENDPOINT || 'https://script.google.com/macros/s/AKfycbyw66RBqZEN6-QDUB96hqpbbQxEDZEDiMg8YDNBVn0oxHncjy8LQ2Braq-yNEws7422zg/exec',
 };
